d33a67b63b4f1248379a06968b347897fa35a8c1
Tylor Hess <tylor@mit.edu>
2013-07-12 13:07:52 +0000

    mergeSort almost written

--- mergesort.js
+++ mergesort.js
@@ -1,3 +1,12 @@
 #!/usr/bin/env node
 
-var x = 1;
+var mergeSort = function(array) {
+    if (array.length <= 1)
+        return array;
+    else {
+        var halfIndex = (array.length / 2) - 1;
+        var sortedArray1 = mergeSort(array1);
+        var sortedArray2 = mergeSort(array2);
+        return merge(sortedArray1,sortedArray2);
+    }
+}
